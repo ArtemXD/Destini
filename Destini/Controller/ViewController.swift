4a53ec6a485a559dc42faf50382594d5b5cf446c
Artem <golynets04@gmail.com>
2023-02-05 23:16:12 +0300

    complited

--- Destini/Controller/ViewController.swift
+++ Destini/Controller/ViewController.swift
@@ -9,11 +9,28 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var questionLabel: UILabel!
+    
+    
+    @IBOutlet weak var secondButton: UIButton!
+    
+    @IBOutlet weak var firstButton: UIButton!
+    
+    var storyBrain = StoryBrain()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        questionLabel.text = storyBrain.storyQuestion[0].title
+        firstButton.setTitle(storyBrain.storyQuestion[0].choice1, for: .normal)
+        secondButton.setTitle(storyBrain.storyQuestion[0].choice2, for: .normal)
     }
 
-
+    @IBAction func clickButton(_ sender: UIButton) {
+        storyBrain.answerCheck((sender.titleLabel?.text)!)
+        questionLabel.text = storyBrain.returnQuestion()
+        firstButton.setTitle(storyBrain.returnChoice1(), for: .normal)
+        secondButton.setTitle(storyBrain.returnChoice2(), for: .normal)
+    }
+    
 }
 
